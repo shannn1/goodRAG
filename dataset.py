6f0b35b2116e45c2950bb914b9d9dd1e82de56ab
Liyang-Zheng <143959715+Liyang-Zheng@users.noreply.github.com>
2024-11-19 16:17:39 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 splits = {'train': 'data/train-00000-of-00001-ec54fbe500fc3b5c.parquet', 'validation': 'data/validation-00000-of-00001-3cf888b12fff1dd6.parquet'}
-df = pd.read_parquet("hf://datasets/lucadiliello/newsqa/" + splits["validation"])
+df_original = pd.read_parquet("hf://datasets/lucadiliello/newsqa/" + splits["validation"])
 
 df = df_original[['context', 'question', 'answers']].copy()
 df['answers'] = df['answers'].apply(lambda x: x[0] if x else None)
