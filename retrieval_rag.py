a18237fd9f9d64006d34c3a35e2fb7109557349d
Liyang-Zheng <143959715+Liyang-Zheng@users.noreply.github.com>
2024-12-12 12:47:33 -0500

    Update retrieval_rag.py

--- retrieval_rag.py
+++ retrieval_rag.py
@@ -14,9 +14,9 @@ from typing import Iterable, List, Optional, Tuple
 
 import numpy as np
 
-from ...tokenization_utils import PreTrainedTokenizer
-from ...tokenization_utils_base import BatchEncoding
-from ...utils import cached_file, is_datasets_available, is_faiss_available, logging, requires_backends, strtobool
+from transformers.tokenization_utils import PreTrainedTokenizer
+from transformers.tokenization_utils_base import BatchEncoding
+from transformers.utils import cached_file, is_datasets_available, is_faiss_available, logging, requires_backends, strtobool
 from .configuration_rag import RagConfig
 from .tokenization_rag import RagTokenizer
 
