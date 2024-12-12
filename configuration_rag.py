aa1a2c177fc4aaca1466ba417afd766eb401b00f
Liyang-Zheng <143959715+Liyang-Zheng@users.noreply.github.com>
2024-12-12 14:39:18 -0500

    Update configuration_rag.py

--- configuration_rag.py
+++ configuration_rag.py
@@ -1,82 +1,6 @@
-# coding=utf-8
-# Copyright 2020, The RAG Authors and The HuggingFace Inc. team.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-"""RAG model configuration"""
-
 from transformers.configuration_utils import PretrainedConfig
 from transformers.utils import add_start_docstrings
 
-
-RAG_CONFIG_DOC = r"""
-    [`RagConfig`] stores the configuration of a *RagModel*. Configuration objects inherit from [`PretrainedConfig`] and
-    can be used to control the model outputs. Read the documentation from [`PretrainedConfig`] for more information.
-
-    Args:
-        title_sep (`str`, *optional*, defaults to  `" / "`):
-            Separator inserted between the title and the text of the retrieved document when calling [`RagRetriever`].
-        doc_sep (`str`, *optional*, defaults to  `" // "`):
-            Separator inserted between the text of the retrieved document and the original input when calling
-            [`RagRetriever`].
-        n_docs (`int`, *optional*, defaults to 5):
-            Number of documents to retrieve.
-        max_combined_length (`int`, *optional*, defaults to 300):
-            Max length of contextualized input returned by [`~RagRetriever.__call__`].
-        retrieval_vector_size (`int`, *optional*, defaults to 768):
-            Dimensionality of the document embeddings indexed by [`RagRetriever`].
-        retrieval_batch_size (`int`, *optional*, defaults to 8):
-            Retrieval batch size, defined as the number of queries issues concurrently to the faiss index encapsulated
-            [`RagRetriever`].
-        dataset (`str`, *optional*, defaults to `"wiki_dpr"`):
-            A dataset identifier of the indexed dataset in HuggingFace Datasets (list all available datasets and ids
-            using `datasets.list_datasets()`).
-        dataset_split (`str`, *optional*, defaults to `"train"`)
-            Which split of the `dataset` to load.
-        index_name (`str`, *optional*, defaults to `"compressed"`)
-            The index name of the index associated with the `dataset`. One can choose between `"legacy"`, `"exact"` and
-            `"compressed"`.
-        index_path (`str`, *optional*)
-            The path to the serialized faiss index on disk.
-        passages_path (`str`, *optional*):
-            A path to text passages compatible with the faiss index. Required if using
-            [`~models.rag.retrieval_rag.LegacyIndex`]
-        use_dummy_dataset (`bool`, *optional*, defaults to `False`)
-            Whether to load a "dummy" variant of the dataset specified by `dataset`.
-        label_smoothing (`float`, *optional*, defaults to 0.0):
-            Only relevant if `return_loss` is set to `True`. Controls the `epsilon` parameter value for label smoothing
-            in the loss calculation. If set to 0, no label smoothing is performed.
-        do_marginalize (`bool`, *optional*, defaults to `False`):
-            If `True`, the logits are marginalized over all documents by making use of
-            `torch.nn.functional.log_softmax`.
-        reduce_loss (`bool`, *optional*, defaults to `False`):
-            Whether or not to reduce the NLL loss using the `torch.Tensor.sum` operation.
-        do_deduplication (`bool`, *optional*, defaults to `True`):
-            Whether or not to deduplicate the generations from different context documents for a given input. Has to be
-            set to `False` if used while training with distributed backend.
-        exclude_bos_score (`bool`, *optional*, defaults to `False`):
-            Whether or not to disregard the BOS token when computing the loss.
-        output_retrieved(`bool`, *optional*, defaults to `False`):
-            If set to `True`, `retrieved_doc_embeds`, `retrieved_doc_ids`, `context_input_ids` and
-            `context_attention_mask` are returned. See returned tensors for more detail.
-        use_cache (`bool`, *optional*, defaults to `True`):
-            Whether or not the model should return the last key/values attentions (not used by all models).
-        forced_eos_token_id (`int`, *optional*):
-            The id of the token to force as the last generated token when `max_length` is reached. Usually set to
-            `eos_token_id`.
-"""
-
-
-@add_start_docstrings(RAG_CONFIG_DOC)
 class RagConfig(PretrainedConfig):
     model_type = "rag"
     is_composition = True
@@ -169,15 +93,7 @@ class RagConfig(PretrainedConfig):
         if self.forced_eos_token_id is None:
             self.forced_eos_token_id = getattr(self.generator, "forced_eos_token_id", None)
 
-    @classmethod
     def from_question_encoder_generator_configs(
         cls, question_encoder_config: PretrainedConfig, generator_config: PretrainedConfig, **kwargs
     ) -> PretrainedConfig:
-        r"""
-        Instantiate a [`EncoderDecoderConfig`] (or a derived class) from a pre-trained encoder model configuration and
-        decoder model configuration.
-
-        Returns:
-            [`EncoderDecoderConfig`]: An instance of a configuration object
-        """
         return cls(question_encoder=question_encoder_config.to_dict(), generator=generator_config.to_dict(), **kwargs)
